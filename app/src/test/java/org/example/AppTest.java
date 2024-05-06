a1794e73b7b4c73de0770b0888aabf8d0d2de7f0
bemayr <bernhard@mayr.io>
2024-05-06 16:47:30 +0200

    fix: remove failing test

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -11,8 +11,4 @@ public class AppTest {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
     }
-
-    @Test public void doesNotWork() {
-        assertEquals(1, 2);
-    }
 }
